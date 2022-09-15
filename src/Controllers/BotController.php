def2d31fd780b2ad659bada70764cb91895f55f0
dzhirnov <dzhirnov@atsaero.ru>
2022-09-15 23:56:49 +0300

    refs#develop Добавил логгирование

--- src/Controllers/BotController.php
+++ src/Controllers/BotController.php
@@ -43,9 +43,15 @@ class BotController
         $token = '5697838884:AAHGcz-ajOtBL-txCiac-WGgHdTct-S1I4k';
         try {
             $bot = new \Longman\TelegramBot\Telegram($token, "DmitryZhirnov");
-
+            $this->logger->info('method = ' . $request->getMethod());
             $message = new Message(json_decode($request->getBody()->getContents(), true));
+            $message->
             $this->logger->info(var_export($message, true));
+            Request::editMessageText([
+                'chat_id' => $message->getChat()->getId(),
+                'message_id' => $message->getMessageId(),
+                'text' => 'new text'
+            ]);
             $bot->handle();
 
         } catch (\Throwable $throwable) {
