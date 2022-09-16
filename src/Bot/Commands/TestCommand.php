54ce712a87799c2740f7bd2a1cc4884ff1d9162a
dzhirnov <dzhirnov@atsaero.ru>
2022-09-16 12:14:41 +0300

    refs#develop Добавил логгирование

--- src/Bot/Commands/TestCommand.php
+++ src/Bot/Commands/TestCommand.php
@@ -5,20 +5,30 @@ namespace App\Bot\Commands;
 use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Request;
 use Longman\TelegramBot\TelegramLog;
+use Psr\Log\LoggerAwareTrait;
 
-class TestCommand extends \Longman\TelegramBot\Commands\Command
+class TestCommand extends \Longman\TelegramBot\Commands\UserCommand
 {
+    use LoggerAwareTrait;
+
     protected $name = 'test';
     protected $usage = '/test';
-
+    protected $version = '1.0.0';
+    protected $description = 'A command for test';
 
     /**
      * @inheritDoc
      */
     public function execute(): ServerResponse
     {
-        TelegramLog::debug(__METHOD__);
-        $text = $this->getMessage()->getText() . ' with test';
-        return $this->replyToChat($text);
+        $message = $this->getMessage();            // Get Message object
+        $chat_id = $message->getChat()->getId();   // Get the current Chat ID
+        $data = [                                  // Set up the new message data
+                                                   'chat_id' => $chat_id,
+                                                   // Set Chat ID to send the message to
+                                                   'text'    => 'This is just a Test...',
+                                                   // Set message to send
+        ];
+        return Request::sendMessage($data);        // Send message!
     }
 }
