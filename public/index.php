55bd711e0e30ef460ab1788d8dd7debc20fa1f4a
dzhirnov <dzhirnov@atsaero.ru>
2022-09-16 18:40:46 +0300

    refs#develop Добавил логгирование

--- public/index.php
+++ public/index.php
@@ -80,3 +80,6 @@ $errorMiddleware->setDefaultErrorHandler($errorHandler);
 $response = $app->handle($request);
 $responseEmitter = new ResponseEmitter();
 $responseEmitter->emit($response);
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
