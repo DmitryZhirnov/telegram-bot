2980520c06c3a85f0f3489da06ea9c31471979ae
dzhirnov <dzhirnov@atsaero.ru>
2022-09-18 20:45:14 +0300

    refs#develop Добавил подключение к БД в модели

--- src/Bot/Services/SpamFilter.php
+++ src/Bot/Services/SpamFilter.php
@@ -19,8 +19,16 @@ class SpamFilter implements ServiceInterface
         $request = json_decode($this->request->getBody()->getContents(), false);
         $text = Str::lower($request->message->text);
         $this->logger->debug(__METHOD__, [json_encode($request->message, JSON_UNESCAPED_UNICODE)]);
-        $this->logger->info(var_export(SwearingWord::query()->where('word', 'LIKE', "%{$text}%")->get(), true));
-        if (SwearingWord::query()->firstWhere('word', 'LIKE', '%' . $text . '%')->exists()) {
+        $swearingWords = SwearingWord::all();
+        $swearingWordExists = false;
+        foreach ($swearingWords as $swearingWord) {
+            $swearingWordExists = Str::contains($swearingWord, $text);
+            if ($swearingWordExists)
+            {
+                break;
+            }
+        }
+        if ($swearingWordExists) {
             $this->logger->info(__METHOD__, ['Удаляю сообщение содержащее матершину']);
             Request::deleteMessage(
                 [
